6201af6d95552e81c4f7dcec17021e6075ec9581
chandru <chandirasegaran.chandru@gmail.com>
2020-03-26 11:37:26 +0530

    PigeonholeSort

---
+++ sorting/PigeonholeSort.py
@@ -0,0 +1,23 @@
+def PigeonholeSort(arr):
+    n = len(arr)
+    min_ = min(arr)
+    max_ = max(arr)
+    range_ = (max_ , max_ - min_ + 1)[min_ < 0]
+    range_ = max(n,range_)
+    p_hole = [0]*range_
+    for i in range(n):
+        p_hole[arr[i]-min_] += 1
+
+    i = 0
+    for count in range(range_):
+        while(p_hole[count]>0):
+            p_hole[count] -= 1
+            arr[i] = count + min_
+            i += 1
+
+
+if __name__ == "__main__":
+    arr = [1,5,3,8,2,6,7,3,9,0]
+    print(arr)
+    PigeonholeSort(arr)
+    print("Sorted Array is {}".format(arr))
